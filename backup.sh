849db71de9aa988384ca5a87795b5107578acb8e
root <root@xiaocai.localdomain>
2018-07-09 21:40:49 +0800

    ss

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ function init{
   git add *
   git commit -m "first commit"
   git remote add origin https://github.com/NetWallow/BackUp.git
-  sed 's/https:\/\/github.com\/NetWallow\/BackUp.git/https:\/\/NetWallow:WANGwang137828@github.com\/NetWallow\/BackUp.git/g' ./.git/config > config
+  sed 's/https:\/\/github.com\/NetWallow\/BackUp.git/https:\/\/<username>:<password>@github.com\/NetWallow\/BackUp.git/g' ./.git/config > config
   mv config ./.git/config
   git push -u origin master
 }
